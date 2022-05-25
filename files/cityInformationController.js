3f8a83ed44a8672fdbc83ce8df310ba04b350176
LenLenTM <91597420+LenLenTM@users.noreply.github.com>
2022-05-25 23:06:51 +0200

    - transformed weather response into a useable object

--- files/cityInformationController.js
+++ files/cityInformationController.js
@@ -1,6 +1,38 @@
 let responseArray = [];
 let paraString;
 let parameter;
+let weatherResponse = [];
+let weather = [];
+let allWeather = [];
+let mainInfo = [];
+let weatherOutput = [];
+
+function WeatherResponse(city, cnt, cod, list, message){
+    this.city = city;
+    this.cnt = cnt;
+    this.cod = cod;
+    this.list = list;
+    this.message = message;
+}
+
+function Weather(clouds, dt, dt_txt, main, pop, rain, sys, visibility, weather, wind){
+    this.clouds = clouds;
+    this.dt = dt;
+    this.dt_txt = dt_txt;
+    this.main = main;
+    this.pop = pop;
+    this.rain = rain;
+    this.sys = sys;
+    this.visibility = visibility;
+    this.weather = weather;
+    this.wind = wind;
+}
+
+function WeatherOutput(date, temp, weather){
+    this.data = date;
+    this.temp = temp;
+    this.weather = weather;
+}
 
 function initPage() {
     navText();
@@ -18,6 +50,10 @@ function navText(){
 
 function getWeather(){
 
+    weatherResponse = [];
+    weather = [];
+    allWeather = [];
+    mainInfo = [];
     let options = {
         method: 'GET',
         headers: {
@@ -29,11 +65,28 @@ function getWeather(){
 
     fetch(url, options)
         .then(response => response.json())
-        /*.then(data => {
-            for (let i = 0; i < data.length; i++) {
-                responseArray.push(new city(data[i].name, data[i].latitude, data[i].longitude, data[i].country, data[i].population));
+        .then(data => {
+            weatherResponse = new WeatherResponse(data.city, data.cnt, data.cod, data.list, data.message);
+            console.log(weatherResponse);
+            for (let i = 1; i < weatherResponse.list.length; i++){
+                weather = new Weather(weatherResponse.list[i].clouds, weatherResponse.list[i].dt, weatherResponse.list[i].dt_txt, weatherResponse.list[i].main, weatherResponse.list[i].pop, weatherResponse.list[i].rain, weatherResponse.list[i].sys, weatherResponse.list[i].visibility, weatherResponse.list[i].weather, weatherResponse.list[i].wind);
+                allWeather.push(weather);
             }
-        }) */
-        .then(response => console.log(response))
+            filterInformation(allWeather);
+
+        })
         .catch(err => console.error(err));
+}
+
+function filterInformation(allWeather){
+    weatherOutput = [];
+    weatherOutput.push(new WeatherOutput(allWeather[0].dt_txt, (allWeather[0].main.temp - 273.15).toFixed(1), allWeather[0].weather[0].main));
+    let date = new Date(weatherOutput[0].data);
+    for (let i = 0; i < allWeather.length; i++){
+        let arrayDate = new Date(allWeather[i].dt_txt);
+        if (arrayDate.getDay() !== date.getDay() && arrayDate.getHours() === 12){
+            weatherOutput.push(new WeatherOutput(allWeather[i].dt_txt, (allWeather[i].main.temp  - 273.15).toFixed(1), allWeather[i].weather[0].main));
+        }
+    }
+    console.log(weatherOutput);
 }
