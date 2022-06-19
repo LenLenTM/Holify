42a67a341f8630bce40244167e291b3cfd41fb73
LenLenTM <91597420+LenLenTM@users.noreply.github.com>
2022-06-19 14:02:48 +0200

    few changes

--- files/login.js
+++ files/login.js
@@ -1,19 +1,9 @@
-let tagArray = [];
-
-class Blog {
-    constructor(title, content, tags, user) {
-
-        this.title = title;
-        this.content = content;
-        this.tags = tags;
-        let date = new Date();
-        this.time = date.toLocaleString("at-DE");
-        this.user = user;
-    }
-}
-
-function initEditorPage(){
+function initLogin(){
     checkCookie();
+
+    let pane = document.createElement('div');
+
+
 }
 
 function checkCookie(){
@@ -40,34 +30,4 @@ function checkCookie(){
                     document.getElementById('nav').append(username);
                 }
             })})
-}
-
-function collectData() {
-    for(let i = 0; i < document.getElementsByClassName('tag').length; i++) {
-        tagArray.push(document.getElementsByClassName('tag').item(i).firstChild.nodeValue);
-        console.log(tagArray);
-    }
-
-    return new Blog(
-        document.getElementById('title').value,
-        tinyMCE.activeEditor.getContent(),
-        tagArray,
-        document.getElementById('user').value
-    );
-}
-
-function blogToJSON(string) {
-    let test = JSON.stringify(string)
-    console.log(test)
-    return test;
-
-
-}
-
-function saveJSON (JSON) {
-    localStorage.setItem('blogPost', JSON);
-}
-
-function jsonToBlog (json) {
-    return JSON.parse(json);
 }
