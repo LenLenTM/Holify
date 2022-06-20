56cd9bc0dbc3ff60851ec3f5246cf6fc4a07bd03
LenLenTM <91597420+LenLenTM@users.noreply.github.com>
2022-06-20 22:31:25 +0200

    some more fixes

--- files/desktopMain.js
+++ files/desktopMain.js
@@ -385,8 +385,10 @@ function createMarker(responseArray, boolean) {
 }
 
 function removeMarker(){
-    for (let i = 0; i < 5; i++){
-        markerList[i].setMap(null);
+    if(markerList.length !== 0) {
+        for (let i = 0; i < 5; i++) {
+            markerList[i].setMap(null);
+        }
     }
 }
 
